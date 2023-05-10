f1eeae072ed1985c8dd55a8b2ca1cf86726b8846
Sergey Zakharov <sergey.v.zakharov@rtlabs.ru>
2023-05-10 13:26:42 +0300

    refactor

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import Types from "../types";
 import User from "../database/shema";
 import * as jwt from "jsonwebtoken";
+import * as bodyParser from "body-parser";
+const cors = require("cors");
 require("dotenv").config();
 require("../database").connect();
 const { API_PORT, TOKEN_KEY } = process.env;
@@ -12,18 +14,23 @@ const auth = require("../middleware/auth");
 
 const app = express();
 
-const urlencodedParser = express.urlencoded({ extended: false });
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
+const jsonParser = bodyParser.json();
+
+app.use(urlencodedParser);
+app.use(jsonParser);
+app.use(cors({ origin: "*", methods: ["GET", "POST"] }));
 
 app.post(
   "/register",
-  urlencodedParser,
-  (req: Request<Types.UserPayload>, res: Response) => {
-    if (!req.body) return res.sendStatus(400).send("Bad request");
-    User.exists({ email: req.body.email }).then((user) => {
+  function (req: Request<Types.UserPayload>, res: Response) {
+    if (!req.body) return res.sendStatus(400).json({ message: `Bad request ` });
+
+    return User.exists({ email: req.body.email }).then((user) => {
       if (user) {
-        return res
-          .status(400)
-          .send(`User with email ${req.body.email} already, exist!!!`);
+        return res.status(400).send({
+          message: `Пользователь с адресом почты ${req.body.email} уже зарегистрирован!`,
+        });
       } else {
         const newUser = new User();
         const { name, email, password } = req.body;
@@ -34,62 +41,65 @@ app.post(
         newUser
           .save()
           .then((u) => {
-            res.status(200).send({ name: u.name, email: u.email });
+            return res.status(200).send({ name: u.name, email: u.email });
           })
           .catch((error) => {
-            res.status(400).send({ error: error.message });
+            return res.status(400).send({ error: error.message });
           });
       }
     });
   }
 );
 
-app.post(
-  "/login",
-  urlencodedParser,
-  (req: Request<Types.UserPayload>, res: Response) => {
-    if (!req.body) {
-      return res.status(400).send("Bad request");
-    }
-    if (!(req.body.email && req.body.password)) {
-      res.status(400).send("All input is required");
-    }
-    User.findOne({ email: req.body.email })
-      .then((user) => {
-        if (user) {
-          const validPassword = user.validatePassword(user.password);
-          if (!validPassword) {
-            res.status(400).send({ message: "Password is not valid" });
-          } else {
-            const payload = {
-              id: user._id,
-              name: user.name,
-              email: user.email,
-              password: user.password,
-            };
-            if (TOKEN_KEY) {
-              user.token = jwt.sign(payload, TOKEN_KEY);
-              res.status(200).json({ ...payload, token: user.token });
-            }
+app.post("/login", function (req: Request<Types.UserPayload>, res: Response) {
+  if (!req.body) {
+    return res.status(400).send("Bad request");
+  }
+  if (!(req.body.email && req.body.password)) {
+    res.status(400).send("All input is required");
+  }
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (user) {
+        const validPassword = user.validatePassword(user.password);
+        if (!validPassword) {
+          res.status(400).send({ message: "Password is not valid" });
+        } else {
+          const payload = {
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            password: user.password,
+          };
+          if (TOKEN_KEY) {
+            user.token = jwt.sign(payload, TOKEN_KEY, {
+              expiresIn: "1h",
+            });
+            res.status(200).json({ ...payload, token: user.token });
           }
         }
-      })
-      .catch((error) => {
-        console.error("Fail to login", error);
-      });
-  }
-);
+      }
+    })
+    .catch((error) => {
+      console.error("Fail to login", error);
+    });
+});
 
 app.post(
   "/app",
   urlencodedParser,
   auth,
-  (req: Request<Types.UserPayload>, res: Response) => {
-    console.log("req", req.body);
-    res.status(200).send("Valid token");
+  (req: Request<Types.IUser["token"]>, res: Response) => {
+    if (TOKEN_KEY) {
+      res.status(200).send(req.body);
+    } else {
+      res.status(400).send({ message: "Not have a secret key..." });
+    }
   }
 );
 
-app.listen(3000, () => {
+// User.deleteMany({});
+
+app.listen(API_PORT, () => {
   `Server running on port ${port}`;
 });
